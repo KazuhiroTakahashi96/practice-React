50aa8f30e1eee5c07e0b5c50a686b3c072a5edac
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-03-12 23:59:58 +0900

    userページ作成

---
+++ src/pages/userIndex.tsx
@@ -0,0 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export async function loader() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const users = await res.json();
+  return { users };
+}
+
+const UserIndex = () => {
+  const { users } = useLoaderData();
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id} className="py-1 underline hover:text-blue-400">
+          <Link to={`${user.id}`}>
+            {user.id}: {user.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserIndex;
